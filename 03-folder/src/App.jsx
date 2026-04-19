562d9804ae3d0e2db1445a051a0c50cedf51d74c
Abuubaida-Assad <assaduddin056464@gmail.com>
2026-04-19 17:47:36 +0530

    learning

--- 03-folder/src/App.jsx
+++ 03-folder/src/App.jsx
@@ -1,29 +1,30 @@
 import { useState } from "react";
 import "./index.css";
+import Todo from './Todo'; 
 
-function App() {
-  const [isTextVisible, setTextVisible] = useState(false);
+// function App() {
+//   const [isTextVisible, setTextVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    setTextVisible(!isTextVisible);
-  };
+//   const toggleVisibility = () => {
+//     setTextVisible(!isTextVisible);
+//   };
 
-  return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <button
-        onClick={toggleVisibility}
-        className="px-5 py-2 bg-amber-200 rounded-xl"
-      >
-        Toggle Text
-      </button>
-      <div className="mt-20">
-        {isTextVisible && (
-          <h1 className="text-indigo-700 text-4xl">hello world</h1>
-        )}
-      </div>
+//   return (
+//     <div className="h-screen flex flex-col justify-center items-center">
+//       <button
+//         onClick={toggleVisibility}
+//         className="px-5 py-2 bg-amber-200 rounded-xl"
+//       >
+//         Toggle Text
+//       </button>
+//       <div className="mt-20">
+//         {isTextVisible && (
+//           <h1 className="text-indigo-700 text-4xl">hello world</h1>
+//         )}
+//       </div>
    
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ 04-UI-folder/src/components/Section1/HeroText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-6'>
+      <h3 className='mb-7 text-7xl font-bold '>prospective <br /><span>customer</span><br />segmentation</h3>
+    </div>
+  )
+}
+
+export default HeroText
